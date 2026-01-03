9d11e7ab410fcf7bd9156d06b7004db3733779a6
UMAIRKHAN786-prog <aquahunt382@gmail.com>
2026-01-03 17:52:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Telegram
+BOT_TOKEN = 8532316648:AAFp4drX7QipmI8BALuuBzCVfJhA337p69U
+
+# Supabase
+SUPABASE_URL = ztnesigamzzomcipqsqp
+SUPABASE_KEY = postgresql://postgres:[YOUR-PASSWORD]@db.ztnesigamzzomcipqsqp.supabase.co:5432/postgres
